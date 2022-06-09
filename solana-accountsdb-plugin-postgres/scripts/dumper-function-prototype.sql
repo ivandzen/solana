3e85d156b0404d222d0028f59727c7fc7ff0a11d
Ivan Loboda <lobodatell@yandex.ru>
2022-06-09 13:17:21 +0300

    fix main function

--- solana-accountsdb-plugin-postgres/scripts/dumper-function-prototype.sql
+++ solana-accountsdb-plugin-postgres/scripts/dumper-function-prototype.sql
@@ -1,6 +1,6 @@
 
 -----------------------------------------------------------------------------------------------------------------------
-CREATE OR REPLACE FUNCTION find_slot_on_longest_branch(req_id VARCHAR)
+CREATE OR REPLACE FUNCTION find_slot_on_longest_branch(transaction_slots BIGINT[])
 RETURNS BIGINT
 AS $find_slot_on_longest_branch$
 DECLARE
@@ -27,9 +27,10 @@ BEGIN
     END IF;
     
     -- does current slot contain transaction ?
-    EXECUTE format('SELECT COUNT(*) FROM txn_slots_%I AS txns WHERE txns.slot = $1', req_id)
+    SELECT COUNT(*) 
     INTO num_in_txn_slots
-    USING current_slot;
+    FROM unnest(transaction_slots) AS slot 
+    WHERE slot = current_slot;
     
     -- if yes - it means we found slot with txn 
     -- on the longest branch - return it
@@ -47,11 +48,12 @@ END;
 $find_slot_on_longest_branch$ LANGUAGE plpgsql;
 
 -----------------------------------------------------------------------------------------------------------------------
--- Returns pre-accounts data for given transaction on a given slot
+-- Returns states of given accounts within a given slot
+-- with write versions not much than maximum
 CREATE OR REPLACE FUNCTION get_pre_accounts_one_slot(
     current_slot BIGINT,
     max_write_version BIGINT,
-    in_txn_signature BYTEA
+    transaction_accounts BYTEA[]
 )
 
 RETURNS TABLE (
@@ -66,7 +68,7 @@ AS $get_pre_accounts_one_slot$
 
 BEGIN
   RETURN QUERY 
-    SELECT DISTINCT ON (acc.pubkey)
+    SELECT
       acc.pubkey,
       acc.slot,
       acc.write_version,
@@ -76,69 +78,77 @@ BEGIN
     WHERE
       acc.slot = current_slot
       AND acc.write_version < max_write_version
-      AND acc.pubkey IN
-        (SELECT ta.pubkey
-        FROM transaction_account AS ta
-        WHERE position(in_txn_signature IN ta.signature) > 0)
-    ORDER BY
-      acc.pubkey DESC, acc.write_version DESC;
+      AND acc.pubkey IN (SELECT * FROM unnest(transaction_accounts));
 END;
 $get_pre_accounts_one_slot$ LANGUAGE plpgsql;
 
 -----------------------------------------------------------------------------------------------------------------------
 CREATE OR REPLACE FUNCTION get_pre_accounts_branch(
-  req_id VARCHAR,
   start_slot BIGINT, 
   max_write_version BIGINT,
-  in_txn_signature BYTEA
+  transaction_accounts BYTEA[]
 )
   
-RETURNS BIGINT --first rooted slot
+RETURNS TABLE (
+    pubkey BYTEA,
+    slot BIGINT,
+    write_version BIGINT,
+    signature BYTEA,
+    data BYTEA
+)
 
 AS $get_pre_accounts_branch$
-DECLARE
-  current_slot BIGINT := start_slot;
-  current_slot_status VARCHAR := NULL;
-  num_in_txn_slots INT := 0;
-BEGIN  
-  LOOP
-    -- get status of current slot
-    SELECT s.status 
-    INTO current_slot_status 
-    FROM slot AS s
-    WHERE s.slot = current_slot;
-    
-    -- already on rooted slot - stop iteration
-    IF current_slot_status = 'rooted' THEN
-      RETURN current_slot;
-    END IF;
 
-    EXECUTE format('INSERT INTO 
-                      results_%I(
-                        pubkey, 
-                        slot, 
-                        write_version, 
-                        signature,
-                        data)
-                    SELECT get_pre_accounts_one_slot($1, $2, $3)
-                    ON CONFLICT (pubkey)
-                    DO NOTHING', req_id)
-    USING current_slot, max_write_version, in_txn_signature;
-    
-    -- If no - go further into the past - select parent slot
-    SELECT s.parent
-    INTO current_slot
-    FROM slot AS s
-    WHERE s.slot = current_slot;
-  END LOOP;
+DECLARE
+  branch_slots BIGINT[];
+  
+  
+BEGIN   
+  WITH RECURSIVE parents AS (
+    SELECT 
+        first.slot,
+        first.parent,
+        first.status
+    FROM slot AS first
+    WHERE first.slot = start_slot
+    UNION
+        SELECT 
+            next.slot,
+            next.parent,
+            next.status
+        FROM slot AS next
+        INNER JOIN parents p ON p.parent = next.slot
+  ) 
+  SELECT array_agg(prnts.slot) 
+  INTO branch_slots
+  FROM parents AS prnts;
+   
+  RETURN QUERY
+  SELECT DISTINCT ON (slot_results.pubkey)
+    slot_results.pubkey,
+    slot_results.slot,
+    slot_results.write_version,
+    slot_results.signature,
+    slot_results.data
+  FROM 
+    unnest(branch_slots) AS current_slot,
+    get_pre_accounts_one_slot(
+                current_slot, 
+                max_write_version, 
+                transaction_accounts
+          ) AS slot_results
+  ORDER BY 
+    slot_results.pubkey, 
+    slot_results.slot DESC,
+    slot_results.write_version DESC;
 END;
 $get_pre_accounts_branch$ LANGUAGE plpgsql;
 
 -----------------------------------------------------------------------------------------------------------------------
 CREATE OR REPLACE FUNCTION get_pre_accounts_root(
-    max_slot BIGINT,
+    start_slot BIGINT,
     max_write_version BIGINT,
-    in_txn_signature BYTEA
+    transaction_accounts BYTEA[]
 )
 
 RETURNS TABLE (
@@ -163,12 +173,9 @@ BEGIN
     WHERE
       acc.slot <= start_slot
       AND acc.write_version < max_write_version
-      AND acc.pubkey IN
-        (SELECT ta.pubkey
-        FROM transaction_account AS ta
-        WHERE position(in_txn_signature IN ta.signature) > 0)
+      AND acc.pubkey IN (SELECT * FROM unnest(transaction_accounts))
       ORDER BY
-        acc.pubkey DESC, acc.write_version DESC
+        acc.pubkey DESC, acc.write_version DESC;
 END;
 $get_pre_accounts_root$ LANGUAGE plpgsql;
 
@@ -187,13 +194,11 @@ AS $get_pre_accounts$
 DECLARE
   current_slot BIGINT := NULL;
   max_write_version BIGINT := NULL;
+  transaction_accounts BYTEA[];
+  transaction_slots BIGINT[];
    
 BEGIN  
-  -- create temporary tables
-  EXECUTE format('CREATE TEMP TABLE txn_slots_%I ( 
-                    slot BIGINT 
-                  ) ON COMMIT DROP', req_id);
-
+  -- Create temporary result table
   EXECUTE format('CREATE TEMP TABLE results_%I (
                     pubkey BYTEA PRIMARY KEY,
                     slot BIGINT,
@@ -203,34 +208,36 @@ BEGIN
                   ) ON COMMIT DROP;', req_id);
                   
   -- Query minimum write version of account update
-  SELECT 
-    MIN(acc.write_version)
+  SELECT MIN(acc.write_version)
   INTO max_write_version
   FROM account AS acc
   WHERE position(in_txn_signature in acc.txn_signature) > 0;
- 
+  
+  -- Query all accounts required by given transaction
+  SELECT array_agg(ta.pubkey)
+  INTO transaction_accounts
+  FROM transaction_account AS ta
+  WHERE position(in_txn_signature IN ta.signature) > 0;
   
   -- find all occurencies of transaction in slots
-  EXECUTE format('INSERT INTO txn_slots_%I
-                    SELECT txn.slot
-                    FROM transaction AS txn
-                    WHERE position($1 in txn.signature) > 0', req_id)
-  USING in_txn_signature;
-
+  SELECT array_agg(txn.slot)
+  INTO transaction_slots
+  FROM transaction AS txn
+  WHERE position(in_txn_signature in txn.signature) > 0;
+  
   -- try to find slot that was rooted with given transaction 
-  EXECUTE format('SELECT ts.slot 
-                    FROM txn_slots_%I AS ts
-                    INNER JOIN slot AS s
-                    ON ts.slot = s.slot
-                    WHERE s.status = $1', req_id)
-  USING 'rooted'
-  INTO current_slot;
+  SELECT DISTINCT txn_slot
+  INTO current_slot
+  FROM unnest(transaction_slots) AS txn_slot
+  INNER JOIN slot AS s
+  ON txn_slot = s.slot
+  WHERE s.status = 'rooted';
     
   
   IF current_slot = NULL THEN 
     -- No rooted slot found. It means transaction exist on some not finalized branch. 
     -- Try to find it on the longest one (search from topmost slot down to first rooted slot)
-    SELECT find_slot_on_longest_branch(req_id) INTO current_slot;
+    SELECT find_slot_on_longest_branch(transaction_slots) INTO current_slot;
     IF current_slot = NULL THEN
         -- Transaction not found on the longest branch - it exist somewhere on minor forks.
         -- Return empty list of accounts
@@ -238,28 +245,21 @@ BEGIN
     END IF;
     
     -- Transaction found on the longest branch. Start searching recent states of accounts in this branch
-    -- down to first rooted slot. This search algorithm iterates over parent slots and is slow.                    
-    SELECT get_pre_accounts_branch(
-      req_id, 
-      current_slot, 
-      max_write_version, 
-      in_txn_signature) INTO current_slot;
+    -- down to first rooted slot. This search algorithm iterates over parent slots and is slow.        
+    EXECUTE format('INSERT INTO results_%I      
+                    SELECT * FROM get_pre_accounts_branch($1, $2, $3)
+                    ON CONFLICT (pubkey)
+                    DO NOTHING', req_id)
+    USING current_slot, max_write_version, transaction_accounts;
   END IF;
   
   -- Transaction found on the rooted slot or restoring state on not finalized branch is finished.
   -- Start/Continue restoring state on rooted slots.
-  
-  EXECUTE format('INSERT INTO 
-                      results_%I(
-                        pubkey, 
-                        slot, 
-                        write_version, 
-                        signature,
-                        data)
-                    SELECT get_pre_accounts_root($1, $2, $3)
-                    ON CONFLICT (pubkey)
-                    DO NOTHING', req_id)
-    USING current_slot, max_write_version, in_txn_signature;
+  EXECUTE format('INSERT INTO results_%I
+                  SELECT * FROM get_pre_accounts_root($1, $2, $3)
+                  ON CONFLICT (pubkey)
+                  DO NOTHING', req_id)
+    USING current_slot, max_write_version, transaction_accounts;
     
   RETURN QUERY EXECUTE format('SELECT * FROM results_%I', req_id);
     
